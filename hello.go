9046ca92ec7839888b3ab6cc6de54545a8342b7b
AnnieMizukiTakahashi <mizukitakahashi.design@gmail.com>
2020-09-26 00:39:32 +0900

    edit code

--- hello.go
+++ hello.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"math"
+	"runtime"
 )
 
 // func add(x, y int) int {
@@ -115,17 +115,42 @@ import (
 // }
 
 // if文
-func sqrt(x float64) string {
-	if x < 0 {
-		return sqrt(-x) + "i"
-	}
-	return fmt.Sprint(math.Sqrt(x))
-}
+// func sqrt(x float64) string {
+// 	if x < 0 {
+// 		return sqrt(-x) + "i"
+// 	}
+// 	return fmt.Sprint(math.Sqrt(x))
+// }
 
 // func main() {
 // 	fmt.Println(sqrt(2), sqrt(-5))
 // }
 
+// func pow(x, n, lim float64) float64 {
+// 	if v := math.Pow(x, n); v < lim {
+// 		return v
+// 	} else {
+// 		fmt.Printf("%g >= %g\n", v, lim)
+// 	}
+// 	return lim
+// }
+
+// func main() {
+// 	fmt.Println(
+// 		pow(3, 2, 10),
+// 		pow(3, 3, 20),
+// 	)
+// }
+
+//使ってるosを取得できる(!)
 func main() {
-	fmt.Println(sqrt(2), sqrt(-5))
+	fmt.Print("Go run on ")
+	switch os := runtime.GOOS; os {
+	case "darwin":
+		fmt.Println("OS X")
+	case "linux":
+		fmt.Println("Linux")
+	default:
+		fmt.Printf("%s.\n", os)
+	}
 }
