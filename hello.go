1c51e07ebf74adf7fc7e2c8ce483d9fafa4dc2ca
AnnieMizukiTakahashi <mizukitakahashi.design@gmail.com>
2020-10-05 23:55:07 +0900

    edit code

--- hello.go
+++ hello.go
@@ -203,15 +203,28 @@ import (
 
 // methods
 
+// type Vertex struct {
+// 	X, Y float64
+// }
+
+// func (v Vertex) Abs() float64 {
+// 	return math.Sqrt(v.X*v.X + v.Y*v.Y)
+// }
+
+// func main() {
+// 	v := Vertex{3, 4}
+// 	fmt.Println(v.Abs())
+// }
+
 type Vertex struct {
 	X, Y float64
 }
 
-func (v Vertex) Abs() float64 {
+func Abs(v Vertex) float64 {
 	return math.Sqrt(v.X*v.X + v.Y*v.Y)
 }
 
 func main() {
 	v := Vertex{3, 4}
-	fmt.Println(v.Abs())
+	fmt.Println(Abs(v))
 }
